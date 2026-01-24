641e30bf0c3b03acb74b752d243572c9cab6f5c9
Prueba01 <prueba0001trabajo@gmail.com>
2026-01-23 21:25:40 -0600

    cambios 1.3

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Edgar + Eduardo</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 3)}>
           count is {count}
